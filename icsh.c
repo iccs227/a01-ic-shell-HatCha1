7f803bbf03dea0ead395c9863c3ec112c00c88a4
HatCha1 <pasu.kos@student.mahidol.edu>
2025-04-25 13:34:57 +0000

    First Milestone

--- icsh.c
+++ icsh.c
@@ -1,17 +1,107 @@
 /* ICCS227: Project 1: icsh
- * Name:
- * StudentID:
+ * Name: Pasu Kositanont
+ * StudentID: 6680060
  */
 
 #include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
 
 #define MAX_CMD_BUFFER 255
 
+char* firstWord(char* buffer);
+void allCommands(char* buffer);
+void cmdEcho(char* buffer);
+void cmdBang();
+void cmdExit(char* buffer);
+
+char prevBuffer[255];
+
 int main() {
     char buffer[MAX_CMD_BUFFER];
+    
+    printf("Starting IC shell\n");
     while (1) {
         printf("icsh $ ");
         fgets(buffer, 255, stdin);
-        printf("you said: %s\n", buffer);
+
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        allCommands(buffer);
     }
+}
+
+char* firstWord(char* buffer){ //https://stackoverflow.com/questions/42605304/how-do-i-split-a-string-at-the-first-space-and-consign-the-strings-to-different
+    char* startOfSecond = strchr(buffer, ' '); // " world";
+    size_t lengthOfFirst;
+
+    if (startOfSecond != NULL) {
+        lengthOfFirst = startOfSecond - buffer;
+    } else {
+        lengthOfFirst = strlen(buffer);
+    }
+
+    char* command = (char*)malloc((lengthOfFirst + 1) * sizeof(char));
+    strncpy(command, buffer, lengthOfFirst);
+    command[lengthOfFirst] = '\0';
+    return command;
+}
+
+void allCommands(char* buffer){
+
+    char* command = firstWord(buffer); //Getting the command line
+
+    if(strlen(command) == 0){ //If type nothing
+        return;
+    }
+
+    if(strcmp(command, "echo") == 0){ //Cmd: echo ...
+        strcpy(prevBuffer, buffer);
+        cmdEcho(buffer);
+    }
+
+    else if(strcmp(command, "!!") == 0){ //Cmd: !!
+        cmdBang();
+    }
+
+    else if(strcmp(command, "exit") == 0){ //Cmd: exit ..
+        free(command);
+        cmdExit(buffer);
+    }
+
+    else{
+        printf("bad command\n"); //Cmd: bad command
+    }
+
+    free(command);
+
+}
+
+void cmdEcho(char* buffer){
+    char* startOfSecond = strchr(buffer, ' ');
+
+    if (startOfSecond[0] == ' ') //https://stackoverflow.com/questions/4295754/how-to-remove-first-character-from-c-string
+        memmove(startOfSecond, startOfSecond+1, strlen(startOfSecond));
+
+    printf("%s\n", startOfSecond);
+}
+
+void cmdBang(){
+    if (strlen(prevBuffer) == 0){
+        return;
+    }
+    printf("%s\n", prevBuffer);
+    allCommands(prevBuffer);
+}
+
+void cmdExit(char* buffer){
+    char* startOfSecond = strchr(buffer, ' ');
+
+    if (startOfSecond[0] == ' ') //https://stackoverflow.com/questions/4295754/how-to-remove-first-character-from-c-string
+        memmove(startOfSecond, startOfSecond+1, strlen(startOfSecond));
+
+    int code = atoi(startOfSecond);
+    code = code & 0xFF;
+    printf("bye\n");
+    exit(code);
 }
